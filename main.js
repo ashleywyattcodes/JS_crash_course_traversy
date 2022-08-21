dc32c9a1e291fa0723bb26fc5c9fa3e600b87b13
Ashley Wyatt <67844566+ashleywyattcodes@users.noreply.github.com>
2022-08-21 18:20:55 -0500

    main.js

--- main.js
+++ main.js
@@ -307,5 +307,86 @@ console.log(person2.getFullName());
         console.log(person1);
 */
 
-// DOM 
+// DOM (document object model)
+/*
 
+// SINGLE ELEMENT SELECTORS 
+    //can be written like this 
+console.log(document.getElementById('my-form'));
+    // or like this 
+const form = document.getElementById('my-form');
+console.log(form);
+   // query selector 
+   console.log(document.querySelector('.container'));
+// MULTIPLE ELEMENT SELECTORS
+console.log(document.querySelectorAll('.item'));
+
+// older method that you would have to convert to an array to get array methods on it. Not recommended 
+
+//  console.log(document.getElementsByClassName('item'));
+//  console.log(document.getElementsByTagName('li'));
+
+// how to loop through 
+
+const items = document.querySelectorAll('.item'); 
+
+items.forEach((item) => console.log(item));
+*/
+
+
+// DOM MANIPULATION
+/*
+const ul = document.querySelector('.items'); 
+
+ul.remove ();
+ul.lastElementChild.remove();
+ul.firstElementChild.textContent = 'Hello';
+ul.children[1].innerText = 'Brad';
+ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
+
+const btn = document.querySelector('.btn');
+btn.style.background='red';
+*/
+// EVENTS 
+/*
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', (e) => {
+    //prevent default behavior so it submit btn does not rapidly flash 
+    console.log('click')
+    console.log(e.target.className);
+    document.querySelector('#my-form').
+    e.preventDefault();
+    // mouseover event to change when you are over the item with your mouse. mouseout reacts when you leave the item 
+    document.querySelector('#my-form').style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+});
+*/
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    
+//form validation, doesnt submit unless the name AND email are filled out. 
+    if(nameInput.value === '' || emailInput.value === ''){
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+        setTimeout(() => msg.remove(), 3000);
+    }else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        userList.appendChild(li);
+
+        //clear fields 
+        nameInput.value = '';
+        emailInput.value= '';
+    }
+}
